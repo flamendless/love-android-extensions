0c3f970280241d86a65c4619c8892363b5ce8d39
flamendless <flamendless8@gmail.com>
2025-06-10 14:11:33 +0800

    Greatly improve script

--- build.sh
+++ build.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
 set -euf -o pipefail
-set -x
 
+GHPATH="${GHPATH:-$HOME/GoingHome}"
 ks="ghr.keystore"
 
+checkver () {
+    echo "Checking version..."
+    local gameversion=$(rg -S "MOBILE_VERSION" ${GHPATH}/game/main.lua -m 1 | cut -d '=' -f 2 | sed -E 's/.*"([0-9]+)d".*/\1/' | xargs)
+    local gradleversion=$(rg -S "app.version_code" gradle.properties -m 1 | cut -d '=' -f 2 | xargs)
+    echo "Found game version '${gameversion}', gradle version '${gradleversion}'"
+
+    if [ ! "${gameversion}" == "${gradleversion}" ]; then
+        echo "Version mismatch. Exiting..."
+        exit 1
+    fi
+}
+
 cplove () {
-    cp ~/GoingHome/release/GoingHomeRevisited.love app/src/embed/assets/game.love
+    set -x
+    cp ${GHPATH}/release/GoingHomeRevisited.love app/src/embed/assets/game.love
 }
 
 sign_apk () {
@@ -18,15 +31,18 @@ sign_aab () {
 }
 
 apk () {
+    set -x
     cplove && sudo ./gradlew assembleEmbedNoRecordRelease && sign_apk
 }
 
 aab () {
+    set -x
     cplove && sudo ./gradlew bundleEmbedNoRecordRelease && sign_aab
 }
 
 if [ "$#" -eq 0 ]; then
     echo "wrong usage"
 else
+    checkver
     "$1" "$@"
 fi
